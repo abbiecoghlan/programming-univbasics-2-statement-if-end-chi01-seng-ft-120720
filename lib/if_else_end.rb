bda06acb4a3dcfa92e374d0be481516e63ec0abd
Abbie Coghlan <abbie.coghlan@gmail.com>
2020-10-26 20:39:31 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,2 +1,8 @@
 # Write your solution here
-if < Time.now.strftime("%S") % 
+if <Time.now.strftime("%S") % 2 == 0> 
+  puts "Even!"
+end
+
+if <Time.now.strftime("%S") % 2 != 0> 
+  puts "Odd!"
+end
